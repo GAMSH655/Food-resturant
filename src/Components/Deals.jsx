a86b646a3e089d66752317e8b4ae1e229512a637
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-02-06 04:50:30 -0800

    added carousel sliding

--- src/Components/Deals.jsx
+++ src/Components/Deals.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
-
+import ChefBurger from "../assets/ChefBurger.png"
+import Grand from "../assets/Grand.png"
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 const Deals = () => {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 6 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 768 },
+          items: 3,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 768, min: 464 },
+          items: 3,
+          slidesToSlide: 2 // optional, default to 1.
+        }
+      };
   return (
-    <div>Deals</div>
+    <div>
+        <div className="flex justify-between p-[30px]">
+            <h3 className="md:text-[32px] font-bold">
+                 up to -40%🎊 order.uk  deals 
+            </h3>
+            <div className="hidden md:flex">
+               <a href="#" className="capitalize p-[8px] hover:border-[2px] hover:border-[#FC8A06] rounded-full hover:font-bold hover:text-[#FC8A06] w-[180px] m-[10px]">vegan</a>
+               <a href="#" className="capitalize p-[8px]hover:border-[2px] hover:border-[#FC8A06] rounded-full hover:font-bold hover:text-[#FC8A06] m-[10px]">sushi</a>
+               <a href="#" className="capitalize p-[8px] hover:border-[2px] hover:border-[#FC8A06] rounded-full hover:font-bold hover:text-[#FC8A06] m-[10px]">pizza & fastfood </a>
+               <a href="#" className="capitalize p-[8px]hover:border-[2px] hover:border-[#FC8A06] rounded-full hover:font-bold hover:text-[#FC8A06] m-[10px]">others</a>
+            </div>
+        </div>
+        <div className="parent">
+      <Carousel
+        responsive={responsive}
+        autoPlay={false}
+        swipeable={true}
+        draggable={true}
+        showDots={false}
+        infinite={true}
+        partialVisible={false}
+        dotListClass="custom-dot-list-style"
+      >
+       <img src={ChefBurger} alt="" className='m-[20px]'  />
+       <img src={Grand} alt=""   className='m-[20px]'  />
+       <img src={ChefBurger} alt=""   className='m-[20px]'  />
+       <img src={Grand} alt=""   className='m-[20px]'  />
+       <img src={ChefBurger} alt=""   className='m-[20px]'  />
+       <img src={Grand} alt=""   className='m-[20px]'  />
+      </Carousel>
+    </div>
+    </div>
   )
 }
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,15 +6,15 @@ import Notification from "../assets/Notification.png"
 import '../assets/Hero.css'
 const Hero = () => {
   return (
-    <div className="block md:flex md:justify-between bg-[#FBFBFB] m-[0px] md:m-[30px] rounded-md shadow-md overflow-hidden">
+    <div className="block md:flex md:justify-between md:text-[#03081F] text-white bg-[#03081F] md:bg-[#E2E2E2] m-[10px] md:m-[30px] rounded-md shadow-md overflow-hidden">
     {/* Left Content */}
     <div className="w-full md:w-1/2 mt-[50px] p-6">
-      <p className="text-[#03081F] text-[1.5rem]">Order Restaurant food, takeaway and groceries.</p>
-      <h3 className="text-[#03081F] font-bold text-[4rem] capitalize">
+      <p className="text-center md:text-left text-[1.5rem]">Order Restaurant food, takeaway and groceries.</p>
+      <h3 className="] text-center md:text-left font-bold text-[1.5rem] md:text-[4rem] capitalize md:p-[0px] p-[10px]">
         Feast Your Senses,
-        <span className="text-[#FC8A06] block">Fast and Fresh</span>
+        <span className="text-[#FC8A06]  text-center md:text-left block">Fast and Fresh</span>
       </h3>
-      <p className="text-[#03081F]">Enter a postcode to see what we deliver</p>
+      <p className="">Enter a postcode to see what we deliver</p>
 
       {/* Search Bar */}
       <div className="flex items-center border border-gray-300 rounded-full overflow-hidden max-w-md mt-4">
@@ -31,7 +31,7 @@ const Hero = () => {
 
     {/* Right Image Section */}
     <div className="w-full md:w-1/2 flex justify-end items-center">
-      <img src={CheeseLady} alt="Cheese Lady" className="max-w-full h-auto object-contain" />
+      <img src={CheeseLady} alt="Cheese Lady" className="max-w-full h-auto object-contain md:block hidden" />
     </div>
     
   </div>
