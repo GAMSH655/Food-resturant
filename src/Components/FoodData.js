367e3563139b0b6499e405953d4c946f9754acb1
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-02-06 07:19:37 -0800

    BREAK

--- src/Components/FoodData.js
+++ src/Components/FoodData.js
@@ -2,7 +2,7 @@ import Soup from "../../src/assets/Soup.png"
 import Pasta from "../assets/Pasta.png"
 import Pizza from "../assets/Pizza.png"
 import Salad from "../assets/Salad.png"
-import BreakFast from "../assets/BreakFast.png"
+import Break from "../assets/Break.png"
 import Burger from "../assets/Burger.png"
 
 const foodData = [
@@ -10,7 +10,7 @@ const foodData = [
  {foodImg:Pasta, FoodName:"Pasta" , ResturantNums:"32 resturants"},
  {foodImg:Pizza, FoodName:"Pizza" , ResturantNums:"4 resturants"},
  {foodImg:Salad, FoodName:"Salad" , ResturantNums:"32 resturants"},
- {foodImg:BreakFast, FoodName:"BreakFast" , ResturantNums:"36 resturants"},
+ {foodImg:Break, FoodName:"BreakFast" , ResturantNums:"36 resturants"},
  {foodImg:Burger, FoodName:"Burger" , ResturantNums:" 40 resturants"},
 ]
 
