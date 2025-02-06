cffd489363f9b1a381e614b21bc1d7bd587a192c
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-02-06 07:27:39 -0800

    working

--- src/Components/Deals.jsx
+++ src/Components/Deals.jsx
@@ -6,21 +6,23 @@ import foodData from '../../src/Components/FoodData';
 const Deals = () => {
     const [isOpen, setIsOpen] = useState(false);
     const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 4,
-          slidesToSlide: 4 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 768 },
-          items: 3,
-          slidesToSlide: 3 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 768, min: 464 },
-          items: 3,
-          slidesToSlide: 2 // optional, default to 1.
-        }
+      superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+      }
       };
   return (
     <div>
